b4c0192a72f5d228af14ba82eee88588e218a14f
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-09-19 16:10:35 +0300

    Add Upsertion & required methods

--- DoctorWho.Db/Repositories/IDoctorRepository.cs
+++ DoctorWho.Db/Repositories/IDoctorRepository.cs
@@ -5,4 +5,5 @@ namespace DoctorWho.Db.Repositories;
 public interface IDoctorRepository
 {
     IEnumerable<Doctor> GetDoctors(int pageNumber, int pageSize);
+    Task<int> UpsertDoctorAsync(Doctor doctor);
 }
