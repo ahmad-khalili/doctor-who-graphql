95be8fd97fe85a48335b2235fdecdf98caf7dfac
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-09-19 18:17:33 +0300

    Add Companion repository with its required adding methods

---
+++ DoctorWho.Db/Repositories/CompanionRepository.cs
@@ -0,0 +1,37 @@
+﻿using DoctorWho.Db.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DoctorWho.Db.Repositories;
+
+public class CompanionRepository : ICompanionRepository
+{
+    private readonly DoctorWhoCoreDbContext _context;
+
+    public CompanionRepository(DoctorWhoCoreDbContext context)
+    {
+        _context = context ?? throw new ArgumentNullException(nameof(context));
+    }
+
+    public async Task AddCompanionToEpisodeAsync(int episodeId, Companion companion)
+    {
+        var episode = await _context.Episodes.FirstOrDefaultAsync(e => e.EpisodeId.Equals(episodeId));
+
+        var episodeCompanion = new EpisodeCompanion
+        {
+            Companion = companion,
+            Episode = episode!
+        };
+
+        await _context.EpisodeCompanions.AddAsync(episodeCompanion);
+    }
+    
+    public async Task<bool> EpisodeExistsAsync(int episodeId)
+    {
+        return await _context.Episodes.AnyAsync(e => e.EpisodeId.Equals(episodeId));
+    }
+
+    public async Task<bool> SaveChangesAsync()
+    {
+        return await _context.SaveChangesAsync() >= 0;
+    }
+}
