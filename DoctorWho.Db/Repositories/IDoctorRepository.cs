a083ff9424ab12d186b07853429d3626b7c4b012
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-09-19 13:58:03 +0300

    Remove unnecessary methods & attributes

--- DoctorWho.Db/Repositories/IDoctorRepository.cs
+++ DoctorWho.Db/Repositories/IDoctorRepository.cs
@@ -1,13 +1,8 @@
 ﻿using DoctorWho.Db.Entities;
-using DoctorWho.Db.Services;
 
 namespace DoctorWho.Db.Repositories;
 
 public interface IDoctorRepository
 {
-    Task<(IEnumerable<Doctor>, PaginationMetadata)> GetDoctorsAsync(int pageNumber, int pageSize);
-    Task<Doctor?> GetDoctorAsync(int doctorId);
-    Task<int> UpsertDoctor(Doctor doctor);
-    void DeleteDoctor(Doctor doctor);
-    Task<bool> SaveChangesAsync();
+    IEnumerable<Doctor> GetDoctors(int pageNumber, int pageSize);
 }
