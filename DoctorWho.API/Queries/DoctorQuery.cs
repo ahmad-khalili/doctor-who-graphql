b52b97ea3b65a1c3a7de5146695a5eb9ca588012
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-09-19 14:00:21 +0300

    Add DoctorType & DoctorQuery classes

---
+++ DoctorWho.API/Queries/DoctorQuery.cs
@@ -0,0 +1,29 @@
+﻿using DoctorWho.API.Types;
+using DoctorWho.Db;
+using DoctorWho.Db.Repositories;
+using GraphQL;
+using GraphQL.Execution;
+using GraphQL.Types;
+
+namespace DoctorWho.API.Queries;
+
+public sealed class DoctorQuery : ObjectGraphType
+{
+    public DoctorQuery()
+    {
+        Field<ListGraphType<DoctorType>>("doctors",
+            arguments: new QueryArguments
+            {
+                new QueryArgument<NonNullGraphType<IntGraphType>> { Name = "pageNumber" },
+                new QueryArgument<NonNullGraphType<IntGraphType>> { Name = "pageSize" }
+            },
+            resolve: context =>
+            {
+                var pageNumber = context.GetArgument<int>("pageNumber");
+                var pageSize = context.GetArgument<int>("pageSize");
+                var repository = context.RequestServices.GetRequiredService<IDoctorRepository>();
+                
+                return repository.GetDoctors(pageNumber, pageSize);
+            });
+    }
+}
