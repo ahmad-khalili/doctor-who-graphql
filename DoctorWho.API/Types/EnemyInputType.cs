466a945054a64849378e9ca05359793ea93c3fe4
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-09-19 18:05:10 +0300

    Add Enemy types (input & retrieval)

---
+++ DoctorWho.API/Types/EnemyInputType.cs
@@ -0,0 +1,13 @@
+﻿using DoctorWho.Db.Entities;
+using GraphQL.Types;
+
+namespace DoctorWho.API.Types;
+
+public sealed class EnemyInputType : InputObjectGraphType<Enemy>
+{
+    public EnemyInputType()
+    {
+        Field(e => e.EnemyName);
+        Field(e => e.Description);
+    }
+}
---
+++ DoctorWho.API/Types/EnemyType.cs
@@ -0,0 +1,14 @@
+﻿using DoctorWho.Db.Entities;
+using GraphQL.Types;
+
+namespace DoctorWho.API.Types;
+
+public sealed class EnemyType : ObjectGraphType<Enemy>
+{
+    public EnemyType()
+    {
+        Field(e => e.EnemyId);
+        Field(e => e.EnemyName);
+        Field(e => e.Description);
+    }
+}
