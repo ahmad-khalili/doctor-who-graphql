4ff7ef1360ea8d2e091ce1189722f08fbde15fd8
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-09-19 14:16:55 +0300

    Create a schema class

---
+++ DoctorWho.API/Schemas/DoctorWhoSchema.cs
@@ -0,0 +1,12 @@
+﻿using DoctorWho.API.Queries;
+using GraphQL.Types;
+
+namespace DoctorWho.API.Schemas;
+
+public class DoctorWhoSchema : Schema
+{
+    public DoctorWhoSchema(IServiceProvider provider) : base(provider)
+    {
+        Query = provider.GetRequiredService<DoctorWhoQuery>();
+    }
+}
