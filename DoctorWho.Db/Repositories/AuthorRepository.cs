1a0488109ab452c1f9b29f9e98617fb775e4cacb
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-09-19 18:37:28 +0300

    Create Author repository with required update methods

---
+++ DoctorWho.Db/Repositories/AuthorRepository.cs
@@ -0,0 +1,33 @@
+﻿using DoctorWho.Db.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DoctorWho.Db.Repositories;
+
+public class AuthorRepository : IAuthorRepository
+{
+    private readonly DoctorWhoCoreDbContext _context;
+
+    public AuthorRepository(DoctorWhoCoreDbContext context)
+    {
+        _context = context ?? throw new ArgumentNullException(nameof(context));
+    }
+
+    public async Task<Author> UpdateAuthorAsync(int authorId, Author author)
+    {
+        var oldAuthor = await _context.Authors.FirstOrDefaultAsync(a => a.AuthorId.Equals(authorId));
+
+        oldAuthor!.AuthorName = author.AuthorName;
+
+        return oldAuthor;
+    }
+
+    public async Task<bool> AuthorExistsAsync(int authorId)
+    {
+        return await _context.Authors.AnyAsync(a => a.AuthorId.Equals(authorId));
+    }
+
+    public async Task<bool> SaveChangesAsync()
+    {
+        return await _context.SaveChangesAsync() >= 0;
+    }
+}
