3143bfc9c0210a7e08c79c1534c85ca80f1b4d58
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-09-19 17:10:39 +0300

    Add Episode repository interface & concrete class

---
+++ DoctorWho.Db/Repositories/IEpisodeRepository.cs
@@ -0,0 +1,8 @@
+﻿using DoctorWho.Db.Entities;
+
+namespace DoctorWho.Db.Repositories;
+
+public interface IEpisodeRepository
+{
+    Task<IEnumerable<Episode>> GetEpisodesAsync(int pageNumber, int pageSize);
+}
