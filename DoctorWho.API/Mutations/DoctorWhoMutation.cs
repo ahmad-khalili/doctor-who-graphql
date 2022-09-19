3e79c493e63f039f0a1e1e1f0585ef06934058ab
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-09-19 16:09:05 +0300

    Create DoctorInputType & Mutation for upserting doctors

---
+++ DoctorWho.API/Mutations/DoctorWhoMutation.cs
@@ -0,0 +1,23 @@
+﻿using DoctorWho.API.Types;
+using DoctorWho.Db.Entities;
+using DoctorWho.Db.Repositories;
+using GraphQL;
+using GraphQL.Types;
+
+namespace DoctorWho.API.Mutations;
+
+public sealed class DoctorWhoMutation : ObjectGraphType
+{
+    public DoctorWhoMutation()
+    {
+        Field<DoctorType>("upsertDoctor").Argument<DoctorInputType>("doctor")
+            .ResolveAsync(async context =>
+            {
+                var repository = context.RequestServices.GetRequiredService<IDoctorRepository>();
+                var doctorToAdd = await context.GetValidatedArgumentAsync<Doctor>("doctor");
+                var addedDoctorId = await repository.UpsertDoctorAsync(doctorToAdd);
+                doctorToAdd.DoctorId = addedDoctorId;
+                return doctorToAdd;
+            });
+    }
+}
