fc28860fd6d22096d0281a336f06f7206d54beff
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-09-19 18:17:06 +0300

    Add Companion types (input & retrieval)

---
+++ DoctorWho.API/Types/CompanionType.cs
@@ -0,0 +1,14 @@
+﻿using DoctorWho.Db.Entities;
+using GraphQL.Types;
+
+namespace DoctorWho.API.Types;
+
+public sealed class CompanionType : ObjectGraphType<Companion>
+{
+    public CompanionType()
+    {
+        Field(c => c.CompanionId);
+        Field(c => c.CompanionName);
+        Field(c => c.WhoPlayed);
+    }
+}
