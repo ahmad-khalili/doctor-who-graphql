d16126b2574904992e34aacf5de812329b012f0c
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-09-19 17:10:55 +0300

    Add an EpisodeType

---
+++ DoctorWho.API/Types/EpisodeType.cs
@@ -0,0 +1,21 @@
+﻿using DoctorWho.Db.Entities;
+using DoctorWho.Db.Repositories;
+using GraphQL.Types;
+
+namespace DoctorWho.API.Types;
+
+public class EpisodeType : ObjectGraphType<Episode>
+{
+    public EpisodeType()
+    {
+        Field(e => e.EpisodeId);
+        Field(e => e.SeriesNumber);
+        Field(e => e.EpisodeNumber);
+        Field(e => e.Title);
+        Field(e => e.EpisodeType);
+        Field(e => e.EpisodeDate);
+        Field(e => e.AuthorId);
+        Field(e => e.DoctorId);
+        Field(e => e.Notes);
+    }
+}
